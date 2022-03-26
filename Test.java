8fed6875cd77347824c8db7c6d5038b0515a26bc
Alex <atiglla@hotmail.com>
2022-03-26 09:29:51 -0500

    PRIMER COMMIT

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Mi nombre es Alex");
+		System.out.println("Tengo 24 años de edad");
+		System.out.println("Mi ciudad natal es Bambamarca, PE");
+	}
+}
+
